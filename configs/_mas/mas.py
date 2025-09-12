228ff2a28786baec3da250df9866662fd401495e
Maciej Stefaniak <madragonse@gmail.com>
2025-09-12 15:50:21 +0200

    dev

---
+++ configs/_mas/mas.py
@@ -0,0 +1,28 @@
+
+
+
+# entropy
+
+# checkponints
+/storage_nvme_3/crewtool/nano_checkpoints/tmp
+
+# importances x8
+
+/storage_nvme_3/crewtool/pc/fw_importances_8m/minitron_dimensions_importances.pt
+/storage_nvme_3/crewtool/pc/fw_importances_8m/random_dimensions_importances.pt
+/storage_nvme_3/crewtool/pc/c4_importances_8m/minitron_dimensions_importances.pt
+/storage_nvme_3/crewtool/pc/c4_importances_8m/random_dimensions_importances.pt
+
+
+# helios
+# checkponints
+/net/scratch/hscra/plgrid/plgmstefaniak/checkpoints/tem
+
+# importances x8
+
+/net/scratch/hscra/plgrid/plgmstefaniak/checkpoints/pc/fw_importances_8m/minitron_dimensions_importances.pt
+/net/scratch/hscra/plgrid/plgmstefaniak/checkpoints/pc/fw_importances_8m/random_dimensions_importances.pt
+/net/scratch/hscra/plgrid/plgmstefaniak/checkpoints/pc/c4_importances_8m/minitron_dimensions_importances.pt
+/net/scratch/hscra/plgrid/plgmstefaniak/checkpoints/pc/c4_importances_8m/random_dimensions_importances.pt
+
+
