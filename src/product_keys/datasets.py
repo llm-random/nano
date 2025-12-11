ef6f6cfac8505053cc508d0faa275c8c7a854977
Mateusz Borowski <m.borowski128@gmail.com>
2025-12-11 16:30:08 +0100

    Add dataset class for Masked Language Modeling

---
+++ src/product_keys/datasets.py
@@ -0,0 +1,23 @@
+from transformers import GPT2TokenizerFast
+
+
+def gpt2_mask_tokenize_fn():
+    tokenizer = GPT2TokenizerFast.from_pretrained("gpt2")
+    additional_special_tokens = [f"<|extra_token_{i}|>" for i in range(46)]
+    tokenizer.add_special_tokens(
+        {
+            "mask_token": "<|mask|>",
+            "additional_special_tokens": additional_special_tokens,
+        }
+    )
+
+    def tokenize_function(examples):
+        texts = [text + tokenizer.eos_token for text in examples["text"]]
+        batch_encodings = tokenizer(
+            texts,
+            truncation=False,
+            max_length=int(1e10),
+        )
+        return batch_encodings
+
+    return tokenize_function
