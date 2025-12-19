255d447126bdafa2d7a49f253fab491cce5aad1e
Maciej Stefaniak <madragonse@gmail.com>
2025-12-19 14:24:02 +0100

    Dev

--- svdllm/component/svd_llama.py
+++ svdllm/component/svd_llama.py
@@ -38,36 +38,36 @@ class LlamaRMSNorm(nn.Module):
         return self.weight * hidden_states
 
 
-# class LlamaRotaryEmbedding(torch.nn.Module):
-#     def __init__(self, dim, max_position_embeddings=2048, base=500000, device=None):
-#         super().__init__()
-#         inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
-#         self.register_buffer("inv_freq", inv_freq)
+class LlamaRotaryEmbedding(torch.nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=500000, device=None):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
+        self.register_buffer("inv_freq", inv_freq)
 
-#         # Build here to make `torch.jit.trace` work.
-#         self.max_seq_len_cached = max_position_embeddings
-#         t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
-#         freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-#         # Different from paper, but it uses a different permutation in order to obtain the same calculation
-#         emb = torch.cat((freqs, freqs), dim=-1)
-#         self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
-#         self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+        # Build here to make `torch.jit.trace` work.
+        self.max_seq_len_cached = max_position_embeddings
+        t = torch.arange(self.max_seq_len_cached, device=self.inv_freq.device, dtype=self.inv_freq.dtype)
+        freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+        # Different from paper, but it uses a different permutation in order to obtain the same calculation
+        emb = torch.cat((freqs, freqs), dim=-1)
+        self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+        self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
 
-#     def forward(self, x, seq_len=None):
-#         # x: [bs, num_attention_heads, seq_len, head_size]
-#         # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
-#         if seq_len > self.max_seq_len_cached:
-#             self.max_seq_len_cached = seq_len
-#             t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype)
-#             freqs = torch.einsum("i,j->ij", t, self.inv_freq)
-#             # Different from paper, but it uses a different permutation in order to obtain the same calculation
-#             emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
-#             self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
-#             self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
-#         return (
-#             self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
-#             self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
-#         )
+    def forward(self, x, seq_len=None):
+        # x: [bs, num_attention_heads, seq_len, head_size]
+        # This `if` block is unlikely to be run after we build sin/cos in `__init__`. Keep the logic here just in case.
+        if seq_len > self.max_seq_len_cached:
+            self.max_seq_len_cached = seq_len
+            t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            # Different from paper, but it uses a different permutation in order to obtain the same calculation
+            emb = torch.cat((freqs, freqs), dim=-1).to(x.device)
+            self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+            self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+        return (
+            self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
+            self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
+        )
 
 
 def rotate_half(x):
@@ -270,14 +270,12 @@ class SVD_LlamaAttention(nn.Module):
         print(f"rope_theta {rope_theta}") #dev
         print(f"Scaling {rope_scaling}") #dev
         # 3. Instantiate the OFFICIAL class with all parameters
-        # self.rotary_emb = LlamaRotaryEmbedding(
-        #     self.head_dim, 
-        #     max_position_embeddings=self.max_position_embeddings,
-        #     base=rope_theta,
-        #     scaling=rope_scaling, # <--- This was missing in your custom class
-        #     device=None
-        # )
-        self.rotary_emb = LlamaRotaryEmbedding(self.config)
+        self.rotary_emb = LlamaRotaryEmbedding(
+            self.head_dim, 
+            max_position_embeddings=self.max_position_embeddings,
+            base=rope_theta
+        )
+        # self.rotary_emb = LlamaRotaryEmbedding(self.config)
 
     def forward(
         self,
@@ -299,7 +297,7 @@ class SVD_LlamaAttention(nn.Module):
         # print(f"past_key_value {past_key_value}") #dev
         # print(f"output_attentions {output_attentions}") #dev
         # print(f"use_cache {use_cache}") #dev
-        
+
         bsz, q_len, _ = hidden_states.size()
 
         # 1. Project Queries (Standard SVD)
