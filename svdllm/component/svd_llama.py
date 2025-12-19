9e2bf1bd963f70eb7afd9714498cfd95cdeaa7ec
Maciej Stefaniak <madragonse@gmail.com>
2025-12-19 13:59:02 +0100

    dev

--- svdllm/component/svd_llama.py
+++ svdllm/component/svd_llama.py
@@ -368,6 +368,8 @@ class SVD_LlamaAttention(nn.Module):
         if not output_attentions:
             attn_weights = None
 
+        print(f"inside lol: {attn_output}")
+
         # return attn_output, attn_weights, past_key_value
         return attn_output, past_key_value
 
