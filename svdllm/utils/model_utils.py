a852623999609c71b78bad2aa9dfc1e089a6afe3
Maciej Stefaniak <madragonse@gmail.com>
2025-12-23 10:48:57 +0100

    dev

--- svdllm/utils/model_utils.py
+++ svdllm/utils/model_utils.py
@@ -16,7 +16,7 @@ def get_model_from_huggingface(model_id):
     # if "opt" in model_id or "mistral" in model_id:
     if True:
         tokenizer = AutoTokenizer.from_pretrained(model_id, device_map="cpu", trust_remote_code=True)
-        # tokenizer.model_max_length = int(1e9)
+        tokenizer.model_max_length = int(1e9)
     else:
         tokenizer = LlamaTokenizer.from_pretrained(model_id, device_map="cpu", trust_remote_code=True)
     model = AutoModelForCausalLM.from_pretrained(model_id, device_map="cpu", torch_dtype=torch.float16, trust_remote_code=True, cache_dir=None)
