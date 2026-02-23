83d9cd87e2b4b187be715c3d3fbfa99a0163f6b3
Wojciech Weremczuk <wojciech.weremczuk5@gmail.com>
2026-02-23 22:51:22 +0100

    Logging config in wandb

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from src.core.checkpointing import (
     load_training_state,
     get_full_checkpoint_path,
 )
-from src.core.metric_loggers import NeptuneLogger, get_metric_logger
+from src.core.metric_loggers import NeptuneLogger, WandbLogger, get_metric_logger
 from src.core.model import Residual
 import platform
 
@@ -216,6 +216,7 @@ def initialize_training_components(cfg: OmegaConf, metric_logger=None):
         metric_logger = get_metric_logger(
             metric_logger_config=cfg.infrastructure.metric_logger,
             tracker_run_id=training_state["run_id"],
+            full_config=cfg,
         )
 
         # Other loggers do not have `run` method
@@ -237,6 +238,17 @@ def initialize_training_components(cfg: OmegaConf, metric_logger=None):
         )
         metric_logger.run["learning_rate"] = learning_rate
         metric_logger.run["exp_lr"] = exp_lr
+
+    elif isinstance(metric_logger, WandbLogger) and (
+        training_state["run_id"] is None
+        or cfg.infrastructure.metric_logger.new_wandb_job
+    ):
+        # Update wandb config
+        metric_logger.run.log({
+            "learning_rate": learning_rate, 
+            "exp_lr": exp_lr, 
+            "full_save_checkpoints_path": get_full_checkpoint_path(cfg.trainer.checkpoint.save.path)
+        })
 
     torch.manual_seed(cfg.trainer.train_dataloader.dataset.seed)
 
