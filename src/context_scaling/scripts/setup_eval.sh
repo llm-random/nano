9ee0894c2c5c36259fdd358b4852792ca0d62ddb
Janek <jan.malasnicki@gmail.com>
2026-04-01 09:02:29 +0200

    fix

--- src/context_scaling/scripts/setup_eval.sh
+++ src/context_scaling/scripts/setup_eval.sh
@@ -25,7 +25,7 @@ export PYTHONPATH="$(pwd):${PYTHONPATH:-}"
 #
 # Optionally updates your sbatch array length if you pass --sbatch_path.
 python src/context_scaling/scripts/setup_eval.py \
-    --tags 2503_grid_namesfix \
+    --tags 2503_grid \
     --negative_tags remove \
     --out_dir 2503_grid_namesfix \
     --sbatch_path src/context_scaling/scripts/eval_models.sbatch
