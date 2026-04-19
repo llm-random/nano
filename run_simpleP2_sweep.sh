c1fa5ece94e7ccca43ac82fba1e5c075509587ff
Janek <jan.malasnicki@gmail.com>
2026-04-19 19:36:03 +0200

    more simpleP runs

--- run_simpleP2_sweep.sh
+++ run_simpleP2_sweep.sh
@@ -12,10 +12,10 @@ for model in "${MODELS[@]}"; do
     pixi run python run_exp.py \
         --config-path=configs/simpleP --config-name="${model}"
 
-    # echo "=== launching vanilla ${model} ==="
-    # pixi run python run_exp.py \
-    #     --config-path=configs/simpleP --config-name="${model}" \
-    #     ~simpleP \
-    #     infrastructure.metric_logger.name="vanilla4_${model}" \
-    #     "infrastructure.metric_logger.tags=[nano, vanilla4, ${model}]"
+    echo "=== launching vanilla ${model} ==="
+    pixi run python run_exp.py \
+        --config-path=configs/simpleP --config-name="${model}" \
+        ~simpleP \
+        infrastructure.metric_logger.name="vanilla2_${model}" \
+        "infrastructure.metric_logger.tags=[nano, vanilla2, ${model}]"
 done
